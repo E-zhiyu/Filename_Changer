731d261d9f50fecb77ab8017b124b432f95bfc62
E-zhiyu <E82487318@outlook.com>
2025-05-01 15:11:33 +0800

    初步完成历史记录界面布局（标题+按钮+滚动区域）

--- FilenameChanger/Fluent_Widgets_GUI/app/view/history_list_interface.py
+++ FilenameChanger/Fluent_Widgets_GUI/app/view/history_list_interface.py
@@ -1,7 +1,8 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QFrame, QWidget, QHBoxLayout, QVBoxLayout
 
-from FilenameChanger.Fluent_Widgets_GUI.qfluentwidgets import (SubtitleLabel)
+from FilenameChanger.Fluent_Widgets_GUI.qfluentwidgets import (SubtitleLabel, PushButton, FluentIcon, setFont,
+                                                               SmoothScrollArea)
 
 
 class HistoryListInterface(QFrame):
@@ -16,6 +17,7 @@ class HistoryListInterface(QFrame):
         self.interfaceLayout = QHBoxLayout(self)
         self.widgetLayout = QVBoxLayout(self.totalWidget)
 
+        self.setLayout(self.interfaceLayout)  # 设置界面主布局
         self.widgetLayout.setAlignment(Qt.AlignmentFlag.AlignTop)  # 默认向上对齐
 
         self.interfaceLayout.addWidget(self.totalWidget)
@@ -23,4 +25,33 @@ class HistoryListInterface(QFrame):
         """标题标签"""
         self.titleLabel = SubtitleLabel(text=text, parent=self)
 
+        setFont(self.titleLabel, 30)
+
         self.widgetLayout.addWidget(self.titleLabel)
+
+        """历史记录编辑按钮"""
+        self.delBtn = PushButton(FluentIcon.DELETE, '删除选中记录')
+        self.clearBtn = PushButton(FluentIcon.BROOM, '清空所有记录')
+        self.btnLayout = QHBoxLayout(self)
+
+        self.btnLayout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+
+        self.btnLayout.addWidget(self.delBtn)
+        self.btnLayout.addWidget(self.clearBtn)
+
+        self.widgetLayout.addLayout(self.btnLayout)
+
+        """历史记录展示区域"""
+        self.historyWidget = QWidget(self)
+        self.historyScrollArea = SmoothScrollArea(self)
+        self.historyLayout = QVBoxLayout(self.historyWidget)
+
+        self.historyLayout.setAlignment(Qt.AlignmentFlag.AlignTop)
+
+        self.historyScrollArea.setWidget(self.historyWidget)
+        self.widgetLayout.addWidget(self.historyScrollArea)
+
+        self.addHistoryCards()  # 加载历史记录卡片
+
+    def addHistoryCards(self):
+        pass
