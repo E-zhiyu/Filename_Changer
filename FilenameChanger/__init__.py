d070350a8ac1d0a6950f7e3da54c924038ce7640
E-zhiyu <E82487318@outlook.com>
2025-05-11 20:32:51 +0800

    修改版本号

--- FilenameChanger/__init__.py
+++ FilenameChanger/__init__.py
@@ -1,6 +1,6 @@
 # python_version == '3.13'
 
-version = "2.2.0"
+version = "2.3.0"
 author = 'GitHub@E-zhiyu'
 config_path = './rename_rules/rename_rules.json'  # 重命名规则文件路径
 history_file_path = './rename_history/rename_history.json'
