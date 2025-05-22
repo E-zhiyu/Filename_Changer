213cd72bc6086566c50642b1247b66b844448be4
E-zhiyu <E82487318@outlook.com>
2025-05-22 09:48:35 +0800

    删除无用配置项

--- FilenameChanger/Fluent_Widgets_GUI/app/common/config.py
+++ FilenameChanger/Fluent_Widgets_GUI/app/common/config.py
@@ -1,30 +1,33 @@
 # coding:utf-8
 import sys
 from enum import Enum
+from pathlib import Path
 
 from PyQt6.QtCore import QLocale
-from FilenameChanger.Fluent_Widgets_GUI.qfluentwidgets import (qconfig, QConfig, ConfigItem, OptionsConfigItem, BoolValidator,
-                            OptionsValidator, RangeConfigItem, RangeValidator,
-                            FolderListValidator, Theme, FolderValidator, ConfigSerializer, __version__)
+from FilenameChanger.Fluent_Widgets_GUI.qfluentwidgets import (qconfig, QConfig, ConfigItem, OptionsConfigItem,
+                                                               BoolValidator,
+                                                               OptionsValidator, RangeConfigItem, RangeValidator,
+                                                               FolderListValidator, Theme, ConfigSerializer,
+                                                               __version__)
 
+from FilenameChanger import config_path
 
-class Language(Enum):
+'''class Language(Enum):
     """ Language enumeration """
 
     CHINESE_SIMPLIFIED = QLocale(QLocale.Language.Chinese, QLocale.Country.China)
     CHINESE_TRADITIONAL = QLocale(QLocale.Language.Chinese, QLocale.Country.HongKong)
     ENGLISH = QLocale(QLocale.Language.English)
-    AUTO = QLocale()
+    AUTO = QLocale()'''
 
-
-class LanguageSerializer(ConfigSerializer):
+'''class LanguageSerializer(ConfigSerializer):
     """ Language serializer """
 
     def serialize(self, language):
         return language.value.name() if language != Language.AUTO else "Auto"
 
     def deserialize(self, value: str):
-        return Language(QLocale(value)) if value != "Auto" else Language.AUTO
+        return Language(QLocale(value)) if value != "Auto" else Language.AUTO'''
 
 
 def isWin11():
@@ -32,38 +35,26 @@ def isWin11():
 
 
 class Config(QConfig):
-    """ Config of application """
+    """应用的设置项"""
 
-    # folders
+    '''# folders
     musicFolders = ConfigItem(
-        "Folders", "LocalMusic", [], FolderListValidator())
+        "Folders", "LocalMusic", [], FolderListValidator())'''
 
     # main window
-    micaEnabled = ConfigItem("MainWindow", "MicaEnabled", isWin11(), BoolValidator())
+    '''micaEnabled = ConfigItem("MainWindow", "MicaEnabled", isWin11(), BoolValidator())'''
     dpiScale = OptionsConfigItem(
         "MainWindow", "DpiScale", "Auto", OptionsValidator([1, 1.25, 1.5, 1.75, 2, "Auto"]), restart=True)
-    language = OptionsConfigItem(
-        "MainWindow", "Language", Language.AUTO, OptionsValidator(Language), LanguageSerializer(), restart=True)
+    '''language = OptionsConfigItem(
+        "MainWindow", "Language", Language.AUTO, OptionsValidator(Language), LanguageSerializer(), restart=True)'''
 
     # Material
-    blurRadius  = RangeConfigItem("Material", "AcrylicBlurRadius", 15, RangeValidator(0, 40))
+    '''blurRadius = RangeConfigItem("Material", "AcrylicBlurRadius", 15, RangeValidator(0, 40))'''
 
     # software update
-    checkUpdateAtStartUp = ConfigItem("Update", "CheckUpdateAtStartUp", True, BoolValidator())
-
-
-YEAR = 2023
-AUTHOR = "zhiyiYo"
-VERSION = __version__
-HELP_URL = "https://qfluentwidgets.com"
-REPO_URL = "https://github.com/zhiyiYo/PyQt-Fluent-Widgets"
-EXAMPLE_URL = "https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/PyQt6/examples"
-FEEDBACK_URL = "https://github.com/zhiyiYo/PyQt-Fluent-Widgets/issues"
-RELEASE_URL = "https://github.com/zhiyiYo/PyQt-Fluent-Widgets/releases/latest"
-ZH_SUPPORT_URL = "https://qfluentwidgets.com/zh/price/"
-EN_SUPPORT_URL = "https://qfluentwidgets.com/price/"
+    '''checkUpdateAtStartUp = ConfigItem("Update", "CheckUpdateAtStartUp", True, BoolValidator())'''
 
 
 cfg = Config()
 cfg.themeMode.value = Theme.AUTO
-qconfig.load('app/config/config.json', cfg)
+qconfig.load(Path(config_path), cfg)
