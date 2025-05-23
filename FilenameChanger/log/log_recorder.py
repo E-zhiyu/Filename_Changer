5b0018a95f5c685921b1e07410beb4d7147605a3
E-zhiyu <E82487318@outlook.com>
2025-05-23 15:30:03 +0800

    将获取日期方法设置为静态

--- FilenameChanger/log/log_recorder.py
+++ FilenameChanger/log/log_recorder.py
@@ -11,30 +11,32 @@ log_dir = './logs'
 
 
 class DailyFileHandler(logging.FileHandler):
-    def __init__(self, log_dir):
+    def __init__(self):
         """
         功能：每天动态生成YYYY-MM-DD.log
         参数 log_dir：日志文件夹路径
         """
-        self.log_dir = log_dir
         os.makedirs(log_dir, exist_ok=True)
         filename = self.get_today_filename()
         super().__init__(filename, encoding='utf-8')
 
-    def get_today_filename(self):
+    @staticmethod
+    def get_today_filename():
         """
         功能：生成当前日期对应的文件名
         """
         today = datetime.now().strftime('%Y-%m-%d')
-        return os.path.join(self.log_dir, f'{today}.log')
+        return os.path.join(log_dir, f'{today}.log')
+
 
 def setup_logger():
     formatter = logging.Formatter('%(asctime)s - [%(filename)s-%(lineno)d/%(levelname)s] : %(message)s')
-    handler = DailyFileHandler(log_dir)
+    handler = DailyFileHandler()
     handler.setFormatter(formatter)
 
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
+
 
 setup_logger()
