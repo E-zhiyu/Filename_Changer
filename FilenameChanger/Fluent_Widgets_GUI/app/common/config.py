218d697b0dac40d27b2bbdc3885b98ac38239616
E-zhiyu <E82487318@outlook.com>
2025-05-24 10:56:28 +0800

    删除无用配置项及注释

--- FilenameChanger/Fluent_Widgets_GUI/app/common/config.py
+++ FilenameChanger/Fluent_Widgets_GUI/app/common/config.py
@@ -1,50 +1,15 @@
 # coding:utf-8
-import sys
 from FilenameChanger.Fluent_Widgets_GUI.qfluentwidgets import (qconfig, QConfig, OptionsConfigItem, OptionsValidator)
 
 from FilenameChanger import config_path
-
-'''class Language(Enum):
-    """ Language enumeration """
-
-    CHINESE_SIMPLIFIED = QLocale(QLocale.Language.Chinese, QLocale.Country.China)
-    CHINESE_TRADITIONAL = QLocale(QLocale.Language.Chinese, QLocale.Country.HongKong)
-    ENGLISH = QLocale(QLocale.Language.English)
-    AUTO = QLocale()'''
-
-'''class LanguageSerializer(ConfigSerializer):
-    """ Language serializer """
-
-    def serialize(self, language):
-        return language.value.name() if language != Language.AUTO else "Auto"
-
-    def deserialize(self, value: str):
-        return Language(QLocale(value)) if value != "Auto" else Language.AUTO'''
-
-
-def isWin11():
-    return sys.platform == 'win32' and sys.getwindowsversion().build >= 22000
 
 
 class Config(QConfig):
     """应用的设置项"""
 
-    '''# folders
-    musicFolders = ConfigItem(
-        "Folders", "LocalMusic", [], FolderListValidator())'''
-
     # main window
     dpiScale = OptionsConfigItem(
         "MainWindow", "DpiScale", "Auto", OptionsValidator([1, 1.25, 1.5, 1.75, 2, "Auto"]), restart=True)
-    '''micaEnabled = ConfigItem("MainWindow", "MicaEnabled", isWin11(), BoolValidator())'''
-    '''language = OptionsConfigItem(
-        "MainWindow", "Language", Language.AUTO, OptionsValidator(Language), LanguageSerializer(), restart=True)'''
-
-    # Material
-    '''blurRadius = RangeConfigItem("Material", "AcrylicBlurRadius", 15, RangeValidator(0, 40))'''
-
-    # software update
-    '''checkUpdateAtStartUp = ConfigItem("Update", "CheckUpdateAtStartUp", True, BoolValidator())'''
 
 
 cfg = Config()
